249e450b4080bd9860b9e5bd7cb04fd39f37d9f8
Tomasz Stachowiak <h3@h3.gd>
2021-07-16 01:25:08 +0200

    Nuke stale code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-//use std::sync::mpsc;
-
 use glam::{EulerRot, Quat, Vec3};
 
 #[derive(Clone, Copy)]
@@ -60,6 +58,12 @@ pub struct YawPitch {
     pub yaw_degrees: f32,
     pub pitch_degrees: f32,
     pub position: Vec3,
+}
+
+impl Default for YawPitch {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl YawPitch {
@@ -125,6 +129,12 @@ pub struct Smooth {
     pub move_smoothness: f32,
     pub look_smoothness: f32,
     pub interp_transform: Option<Transform>,
+}
+
+impl Default for Smooth {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Smooth {
@@ -220,7 +230,7 @@ pub struct CameraRigBuilder {
 }
 
 impl CameraRig {
-    pub fn new() -> CameraRigBuilder {
+    pub fn builder() -> CameraRigBuilder {
         CameraRigBuilder {
             drivers: Default::default(),
         }
@@ -243,232 +253,3 @@ impl CameraRigBuilder {
         rig
     }
 }
-
-/*pub trait RigElement {
-    fn update(&mut self, dt: f32);
-    fn transform(&self) -> Transform;
-}
-
-pub struct FirstPersonCameraInput {
-    pub move_vec: Vec3,
-    pub yaw_delta: f32,
-    pub pitch_delta: f32,
-}
-
-impl FirstPersonCameraInput {
-    pub fn relative_to(self, transform: &Transform) -> Self {
-        FirstPersonCameraInput {
-            move_vec: transform.rotation * self.move_vec,
-            yaw_delta: self.yaw_delta,
-            pitch_delta: self.pitch_delta,
-        }
-    }
-}
-
-pub struct FirstPerson {
-    // Degrees
-    pub yaw: f32,
-    pub pitch: f32,
-    pub roll: f32,
-
-    pub move_speed: f32,
-
-    transform: Transform,
-    input_receiver: mpsc::Receiver<FirstPersonCameraInput>,
-}
-
-impl FirstPerson {
-    pub fn from_transform(transform: Transform) -> (Self, mpsc::Sender<FirstPersonCameraInput>) {
-        let (tx, rx) = mpsc::channel();
-
-        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
-        (
-            Self {
-                yaw: yaw.to_degrees(),
-                pitch: pitch.to_degrees(),
-                roll: roll.to_degrees(),
-                move_speed: 10.0,
-                transform,
-                input_receiver: rx,
-            },
-            tx,
-        )
-    }
-
-    pub fn from_position_rotation(
-        position: Vec3,
-        rotation: Quat,
-    ) -> (Self, mpsc::Sender<FirstPersonCameraInput>) {
-        Self::from_transform(Transform {
-            translation: position,
-            rotation,
-        })
-    }
-
-    pub fn from_position_yaw_pitch_roll(
-        position: Vec3,
-        yaw_degrees: f32,
-        pitch_degrees: f32,
-        roll_degrees: f32,
-    ) -> (Self, mpsc::Sender<FirstPersonCameraInput>) {
-        let (tx, rx) = mpsc::channel();
-        (
-            Self {
-                yaw: yaw_degrees,
-                pitch: pitch_degrees,
-                roll: roll_degrees,
-                move_speed: 10.0,
-                transform: Transform {
-                    translation: position,
-                    rotation: Quat::from_euler(
-                        EulerRot::YXZ,
-                        yaw_degrees.to_radians(),
-                        pitch_degrees.to_radians(),
-                        roll_degrees.to_radians(),
-                    ),
-                },
-                input_receiver: rx,
-            },
-            tx,
-        )
-    }
-}
-
-impl RigElement for FirstPerson {
-    fn update(&mut self, dt: f32) {
-        let input = self.input_receiver.try_recv().unwrap();
-
-        self.transform.translation += input.move_vec.clamp_length_max(1.0) * (self.move_speed * dt);
-
-        self.pitch = (self.pitch + input.pitch_delta).clamp(-90.0, 90.0);
-        self.yaw = (self.yaw + input.yaw_delta) % 720_f32;
-
-        self.transform.rotation = Quat::from_euler(
-            EulerRot::YXZ,
-            self.yaw.to_radians(),
-            self.pitch.to_radians(),
-            self.roll.to_radians(),
-        );
-    }
-
-    fn transform(&self) -> Transform {
-        self.transform
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct InterpolatedDesc {
-    pub move_smoothness: f32,
-    pub look_smoothness: f32,
-}
-
-impl Default for InterpolatedDesc {
-    fn default() -> Self {
-        Self {
-            move_smoothness: 1.0,
-            look_smoothness: 1.0,
-        }
-    }
-}
-
-pub struct Interpolated<T: RigElement> {
-    inner: T,
-    transform: Transform,
-    desc: InterpolatedDesc,
-}
-
-impl<T: RigElement> RigElement for Interpolated<T> {
-    fn update(&mut self, dt: f32) {
-        self.inner.update(dt);
-        let inner_transform = self.inner.transform();
-
-        let rot_interp = 1.0 - (-1.0 * dt / self.desc.look_smoothness.max(1e-5)).exp();
-        let pos_interp = 1.0 - (-1.0 * dt / self.desc.move_smoothness.max(1e-5)).exp();
-
-        self.transform.rotation = self
-            .transform
-            .rotation
-            .slerp(inner_transform.rotation, rot_interp)
-            .normalize();
-
-        self.transform.translation = self
-            .transform
-            .translation
-            .lerp(inner_transform.translation, pos_interp);
-    }
-
-    fn transform(&self) -> Transform {
-        self.transform
-    }
-}
-
-pub trait RigInterpolated: RigElement + Sized {
-    fn interpolated(self, desc: InterpolatedDesc) -> Interpolated<Self>;
-}
-
-impl<T: RigElement> RigInterpolated for T {
-    fn interpolated(self, desc: InterpolatedDesc) -> Interpolated<T> {
-        let inner_transform = self.transform();
-        Interpolated {
-            inner: self,
-            transform: inner_transform,
-            desc,
-        }
-    }
-}
-
-pub struct Arm<T: RigElement> {
-    inner: T,
-    arm_length: f32,
-}
-
-impl<T: RigElement> RigElement for Arm<T> {
-    fn update(&mut self, dt: f32) {
-        self.inner.update(dt);
-    }
-
-    fn transform(&self) -> Transform {
-        let inner_transform = self.inner.transform();
-
-        Transform {
-            rotation: inner_transform.rotation,
-            translation: inner_transform.translation
-                + inner_transform.rotation * Vec3::Z * self.arm_length,
-        }
-    }
-}
-
-pub trait RigArm: RigElement + Sized {
-    fn arm(self, arm_length: f32) -> Arm<Self>;
-}
-
-impl<T: RigElement> RigArm for T {
-    fn arm(self, arm_length: f32) -> Arm<T> {
-        Arm {
-            inner: self,
-            arm_length,
-        }
-    }
-}
-
-#[test]
-fn test() {
-    let (camera, camera_input) = FirstPerson::from_position_rotation(Vec3::ZERO, Quat::IDENTITY);
-    let mut camera = camera.interpolated(InterpolatedDesc::default());
-
-    camera_input
-        .send(FirstPersonCameraInput {
-            move_vec: Vec3::ZERO,
-            yaw_delta: 0.0,
-            pitch_delta: 0.0,
-        })
-        .unwrap();
-    camera.update(1.0 / 60.0);
-
-    let mut camera_rig = CameraRig::new()
-        .with(YawPitch::new())
-        .with(Smooth::new())
-        .build();
-    camera_rig.driver_mut::<YawPitch>();
-}
-*/
